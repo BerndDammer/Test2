aa887a5a2e53322221c976bbcd8db193b37402b2
BerndDammer <BerndDammer@web.de>
2017-07-30 09:40:03 +0200

    push test

---
+++ git_set_remote2.sh
@@ -0,0 +1,9 @@
+git config --remove-section user
+git config user.name BerndDammer
+git config user.email BerndDammer@web.de 
+
+echo ------
+# echo must do manual remove in config
+echo press
+read
+
